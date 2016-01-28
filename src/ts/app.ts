7f3ba560d3834f8d25af5f0387bf1c66a6f1028c
Denis Kolodin <deniskolodin@gmail.com>
2016-01-28 23:21:14 +0300

    Add routing with main page

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,9 +1,16 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {MainPage} from './main';
 
 @Component({
 	selector: 'app',
-	templateUrl: 'app.html'
+	templateUrl: 'app.html',
+	directives: [ROUTER_DIRECTIVES], 
 })
+
+@RouteConfig([
+	{path: '/main', name: 'Main', component: MainPage, useAsDefault: true},
+])
 
 export class AppComponent {
 
---
+++ src/ts/main.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'main',
+	templateUrl: 'main.html',
+})
+
+export class MainPage {
+
+}
+
