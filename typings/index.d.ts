366e0c20caf867c539164578d24821e279f271f4
Denis Kolodin <deniskolodin@gmail.com>
2016-07-17 21:24:36 +0300

    Update typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,3 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/github-electron/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
